75001d50c4c15ea1b66262b4825c3d20a0092977
general-soldatov <general-soldatov@mail.ru>
2024-07-07 00:03:27 +0300

    Add more flask`s and jinja`s element in html

--- app.py
+++ app.py
@@ -6,20 +6,25 @@ app = Flask(__name__)
 def index():
     weddind_date = '11 августа 2024 год'
     week_day = 'Воскресенье'
-    location = 'пос. Новая Усмань, Усадьба 12.24'
+    location = 'с. Новая Усмань, Усадьба 12.24'
     go_to = 10
     waiting = 50
     together_day = 100
     time_kiss = 100
 
-    newlyweds = [
-        ['Юрий Солдатов', 'images/yura.jpeg', 'От студенческой скамьи до преподавательской кафедры.'],
-        ['Эльвира Малюгина', 'images/elya.jpeg', 'Никто не знает о здоровье больше, чем она.']
-    ]
     up_case = [
         ['Home', 'index.html'],
         ['Telegram-канал', '#'],
         ['Цветочная подписка', '#']
+    ]
+    newlyweds = [
+        ['Юрий Солдатов', 'images/yura.jpeg', 'От студенческой скамьи до преподавательской кафедры.'],
+        ['Эльвира Малюгина', 'images/elya.jpeg', 'Никто не знает о здоровье больше, чем она.']
+    ]
+    events = [
+        ['Welcome', 'images/welcome.jpg', '16:00 - 17:00', 'Здесь Вы сможете познакомиться с гостями.'],
+        ['Выездная регистрация', 'images/wedding.jpg', '17:00 - 18:00', 'Здесь начинается семья.'],
+        ['Свадебный ужин', 'images/dinner.jpg', '18:00 - 22:00', 'Такое событие, что не отметить непростительно!']
     ]
     family = [
         ['Игорь Солдатов', 'папа', 'vk', '#', 'images/couple-1.jpg', 'spitch'],
@@ -34,7 +39,7 @@ def index():
                            go_to = go_to, waiting=waiting,
                            together_day=together_day, time_kiss=time_kiss,
                            family=family, up_case=up_case, cal=calendar,
-                           location=location, newlyweds=newlyweds)
+                           location=location, newlyweds=newlyweds, events=events)
 
 if __name__ == '__main__':
     app.run()
