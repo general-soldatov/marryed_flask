478370da337ca6d802639c00cc597a3dcd92cac5
general-soldatov <general-soldatov@mail.ru>
2024-07-07 13:51:53 +0300

    Insert Template inheritance

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template, request, redirect, url_for
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+@app.route('/index')
 @app.route('/', methods=['get', 'post'])
 def index():
     client = ''
