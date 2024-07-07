d2b4a1dc278e3f1f7c3a51ca150910e022d0e407
general-soldatov <general-soldatov@mail.ru>
2024-07-07 13:32:15 +0300

    Add new flask`s element in project

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, flash
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
+    client = ''
     weddind_date = '11 августа 2024 год'
     week_day = 'Воскресенье'
     location = 'с. Новая Усмань, Усадьба 12.24'
@@ -34,7 +37,19 @@ def index():
         ['Валерия Малюгина', 'сестра', 'vk', '#', 'images/couple-2.jpg', 'spitch']
     ]
     calendar = ['Wedding+Soldatov`s', '20240811T130000', '20240811T190000', 'https://example.com/tickets-43251101208', 'Studio 12. 24']
+
+    client_name = ''
+    client_mail = ''
+
+    if request.method == 'POST':
+        client_name = request.form.get("name")
+        client_mail = request.form.get("email")
+        print(client_mail, client_name)
+        flash('Спасибо, будем ждать Вас на мероприятии!')
+        return redirect(url_for('index'))
+
     return render_template('index.html',
+                           client=client,
                            date=weddind_date, week_day=week_day,
                            go_to = go_to, waiting=waiting,
                            together_day=together_day, time_kiss=time_kiss,
